1a64cdeb0b5fbcab6a9df57fd128e9f6b6b10f0f
Neil Thimmaiah <47484641+neilthimmaiah01@users.noreply.github.com>
2026-01-14 16:45:19 -0600

    Create test_code2.py

---
+++ Agno_Test/test_code2.py
@@ -0,0 +1,13 @@
+import requests
+
+url = "https://www.perplexity.ai/hub/blog/building-safer-ai-browsers-with-browsesafe"
+
+try:
+    response = requests.get(url)
+    
+    response.raise_for_status() 
+    html_content = response.text
+    print(html_content)
+
+except requests.exceptions.RequestException as e:
+    print(f"An error occurred: {e}")
