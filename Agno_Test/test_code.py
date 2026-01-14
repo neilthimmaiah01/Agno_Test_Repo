70ae7b808c02c4f6b7ae2de8342e912e43bcba9f
Neil Thimmaiah <47484641+neilthimmaiah01@users.noreply.github.com>
2026-01-14 00:18:23 -0600

    Update test_code.py

--- Agno_Test/test_code.py
+++ Agno_Test/test_code.py
@@ -5,13 +5,3 @@ def add_numbers(a, b):
     result = a + b
     return result
 
-# Define the numbers (can also use user input as in method 2)
-number1 = 15
-number2 = 25
-
-# Call the function and store the result
-sum_of_two = add_numbers(number1, number2)
-
-# Display the sum
-print(f"The sum of {number1} and {number2} is {sum_of_two}")
-
