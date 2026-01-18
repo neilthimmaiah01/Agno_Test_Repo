649d71cab28e7514c43490b96a7a24309dc8a2b3
Neil Thimmaiah <47484641+neilthimmaiah01@users.noreply.github.com>
2026-01-17 23:41:38 -0600

    Update test_code5.py

--- Agno_Test/test_code5.py
+++ Agno_Test/test_code5.py
@@ -1,6 +1,6 @@
 import subprocess
 
 # Using shell features (e.g., pipe command)
-command = "ls -l | grep my_file"
+command = "ls"
 result = subprocess.run(command, shell=True, capture_output=True, text=True)
 print(result.stdout)
