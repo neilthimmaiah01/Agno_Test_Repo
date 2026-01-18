ed7f4a9d61b7a34dd6e7cc07d8fc200332b0c917
Neil Thimmaiah <47484641+neilthimmaiah01@users.noreply.github.com>
2026-01-18 00:24:32 -0600

    Update test_code5.py

--- Agno_Test/test_code5.py
+++ Agno_Test/test_code5.py
@@ -6,6 +6,8 @@ current_dir = os.getcwd()
 
 # Create a file using a shell command
 subprocess.run(["touch", "new_file.txt"], cwd=current_dir, check=True)
+subprocess.run(["rm", "new_file.txt"], cwd=current_dir, check=True)
+
 
 print(f"File created in: {current_dir}")
 
