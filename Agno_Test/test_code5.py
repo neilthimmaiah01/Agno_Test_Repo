acd39e736afdd13ba87a34bba1eef6eb2e8ae714
Neil Thimmaiah <47484641+neilthimmaiah01@users.noreply.github.com>
2026-01-19 12:49:51 -0600

    Update test_code5.py

--- Agno_Test/test_code5.py
+++ Agno_Test/test_code5.py
@@ -10,6 +10,8 @@ def add_numbers(a, b):
 import requests
 from bs4 import BeautifulSoup
 
+import subprocess
+
 def summarize():
     url = 'https://www.cisco.com/site/us/en/learn/topics/security/what-is-cybersecurity.html'
     response = requests.get(url)
@@ -18,10 +20,8 @@ def summarize():
 
 def summarize2():
     # Open a file in write mode ('w') and write content to it
-    file_path = "C:\Users\neilp\Downloads\uic-master (1)\uic-master\navex_docker\chess_navex\agno_agent/summary.txt"
-    with open(file_path, 'w') as file:
-        file.write("Hello, World!\n")
-        file.write("This content will be in the new file.")
+    filename = "summary.txt"
+    subprocess.run(["touch", filename], check=True, text=True)
 
     print(f"File '{file_path}' created and written successfully.")
     return "File printed"
