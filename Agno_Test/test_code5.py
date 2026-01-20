979fa5a83c906bd59b78f27761756237f7d4218c
Neil Thimmaiah <47484641+neilthimmaiah01@users.noreply.github.com>
2026-01-20 10:29:14 -0600

    Update test_code5.py

--- Agno_Test/test_code5.py
+++ Agno_Test/test_code5.py
@@ -8,19 +8,17 @@ def add_numbers(a, b):
     result = a + b
     return result
 
-import requests
-from bs4 import BeautifulSoup
-
-from pathlib import Path
-import os
-
 def summarize():
+    import requests
+    from bs4 import BeautifulSoup
     url = 'https://www.cisco.com/site/us/en/learn/topics/security/what-is-cybersecurity.html'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     return soup.title
 
 def summarize2():
+    from pathlib import Path
+    import os
     # Open a file in write mode ('w') and write content to it
     filename = "summary.txt"
     
